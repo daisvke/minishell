28058db5cda8a7ef4af5efeeb62a6191bd9c619c
dtanigaw <dtanigaw@student.42.fr>
2021-12-25 07:29:29 +0100

    Trying to make ls work with absolute path

--- src/utils_strings.c
+++ src/utils_strings.c
@@ -6,11 +6,23 @@
 /*   By: dtanigaw <dtanigaw@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/15 09:21:17 by dtanigaw          #+#    #+#             */
-/*   Updated: 2021/12/16 03:57:36 by dtanigaw         ###   ########.fr       */
+/*   Updated: 2021/12/25 05:11:12 by dtanigaw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+int	ms_strcmp(const char *s1, const char *s2)
+{
+	while (*s1 || *s2)
+	{
+		if (*s1 != *s2)
+			return ((unsigned char)(*s1) - (unsigned char)(*s2));
+		s1++;
+		s2++;
+	}
+	return (0);
+}
 
 int	ms_strncmp(const char *s1, const char *s2, size_t n)
 {
