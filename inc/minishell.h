3c4b01c8c0b0b7f59c88c375488dc5b63eea6531
dtanigaw <dtanigaw@student.42.fr>
2021-12-27 07:21:41 +0100

    Add exit

--- inc/minishell.h
+++ inc/minishell.h
@@ -6,7 +6,7 @@
 /*   By: dtanigaw <dtanigaw@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/14 06:18:38 by dtanigaw          #+#    #+#             */
-/*   Updated: 2021/12/25 21:40:14 by dtanigaw         ###   ########.fr       */
+/*   Updated: 2021/12/27 06:09:59 by dtanigaw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,7 @@
 
 # define MS_SAME			0
 # define MS_OK				0
+# define MS_ERROR			1
 
 # define MS_LOOP_NOT_ENDED_BY_CTRL_D	1
 
@@ -38,8 +39,6 @@
 # define MS_CMD_UNSET		4
 # define MS_CMD_EXIT		5
 # define MS_FIRST_ARG_POS	1
-
-# define MS_ERROR			1
 
 // Main struct
 typedef struct s_ms
