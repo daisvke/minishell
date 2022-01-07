5a4f1b9ce301af4de863174d3face39c8915d8b5
dtanigaw <dtanigaw@student.42.fr>
2022-01-07 17:56:18 +0100

    from alien

--- inc/minishell.h
+++ inc/minishell.h
@@ -6,7 +6,7 @@
 /*   By: dtanigaw <dtanigaw@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/14 06:18:38 by dtanigaw          #+#    #+#             */
-/*   Updated: 2022/01/03 10:34:46 by dtanigaw         ###   ########.fr       */
+/*   Updated: 2022/01/06 22:49:57 by dtanigaw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,20 +21,16 @@
 # include <signal.h>
 # include <termios.h>
 
-# include "ms_macros_and_structs.h"
+# include "ms_macros_structs_enums.h"
+
 # include "pipex.h"
 
 /*
-** Strings utils
+** utils_strings
 */
 int	ms_strncmp(const char *s1, const char *s2, size_t n);
 
 char	*ms_strdup(char *src, size_t size); //used
 char	**ms_split_and_activate_options(t_ms *env, char const *s, char sep);
-
-/*
-** Linked lists
-*/
-t_env_lst	*ms_lst_create_new_node(char *data);
 
 #endif
