54e0e3891ff696310001920cf43f379ebe931c55
daisvke <daisuketanigawa@live.fr>
2022-01-12 05:44:34 +0100

    Add expansion when variable between double quotes

--- src/get_next_line.c
+++ src/get_next_line.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "minishell.h"
 
 size_t	gnl_get_char_index(char *str, char c, bool increment)
 {
