b2d9faa8666efa33c87e9a07a8278d3c30644cf8
daisvke <daisuketanigawa@live.fr>
2022-01-20 02:41:13 +0100

    implemented functions are ignored when pipe is on

--- src/minishell.c
+++ src/minishell.c
@@ -6,23 +6,17 @@
 /*   By: dtanigaw <dtanigaw@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/16 03:24:27 by dtanigaw          #+#    #+#             */
-/*   Updated: 2022/01/20 00:43:32 by dtanigaw         ###   ########.fr       */
+/*   Updated: 2022/01/20 02:25:11 by dtanigaw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-t_env_lst	g_envp;
-
-int	ms_launch_prompt(char *envp[], t_ms *env, int last_pipe_exit_status)
+void	ms_launch_prompt(t_ms *env)
 {
-	ms_prompt_and_execute_cmd_line_with_pipex(envp, env, last_pipe_exit_status);
-	last_pipe_exit_status = env->last_pipe_exit_status;
-	ms_lst_clear_list(env->envp_lst);
+	ms_prompt_and_execute_cmd_line_with_pipex(env);
 	ppx_free_all_allocated_variables(&env->ppx_env);
 	ppx_free_array_of_pointers(env->split_cmd_line, MS_ALL);
-
-	return (last_pipe_exit_status);
 }
 
 void	ms_run_command_and_quit(int argc, char *argv[], t_ms *env)
@@ -38,21 +32,15 @@ void	ms_run_command_and_quit(int argc, char *argv[], t_ms *env)
 int	main(int argc, char *argv[], char *envp[])
 {
 	t_ms	env;
-	int		last_pipe_exit_status;
 
-	if (argc == 1)
-		ms_init_env(envp, &env, false);
-	else
-		ms_init_env(envp, &env, true);
+	ms_init_env(envp, &env);
 	ms_handle_signals();
 	if (ms_check_arguments(envp) == MS_OK)
 	{
-		last_pipe_exit_status = 0;
 		while (MS_LOOP_NOT_ENDED_BY_CTRL_D)
 		{
 			if (argc == 1)
-				last_pipe_exit_status = ms_launch_prompt(\
-					envp, &env, last_pipe_exit_status);
+				ms_launch_prompt(&env);
 			else
 			{
 				ms_run_command_and_quit(argc, argv, &env);
