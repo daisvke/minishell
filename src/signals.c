c8826bd4e596fb04b696f116a30366f4d3e774b6
daisvke <daisuketanigawa@live.fr>
2022-01-23 23:40:57 +0100

    First read line problem solved

--- src/signals.c
+++ src/signals.c
@@ -6,7 +6,7 @@
 /*   By: dtanigaw <dtanigaw@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/14 07:23:04 by dtanigaw          #+#    #+#             */
-/*   Updated: 2022/01/22 20:21:53 by dtanigaw         ###   ########.fr       */
+/*   Updated: 2022/01/23 21:58:08 by dtanigaw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,6 @@ void	ms_handle_sigint(int signum)
 	(void)signum;
 	rl_replace_line("", 0);
 	write(STDOUT_FILENO, "\n", 1);
-//	write(STDOUT_FILENO, MS_PROMPT, 16);
 	rl_on_new_line();
 	rl_redisplay();
 }
