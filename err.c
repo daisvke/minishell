a4075c5087de204becf91437926f02ebd11c44ef
daisvke <daisuketanigawa@live.fr>
2022-02-06 06:03:15 +0100

    Add colors to cmd prompt and solve readline counting ANSI char as characters

--- err.c
+++ err.c
@@ -1,34 +1,4 @@
-<< eof << eof
-
-/************************************************************
-************************************************************/
-
-$ <<d
-a
-d
-$ mls
-here
-mls: command not found
-$ ls
-here
-a  inc  Makefile  minishell  obj  README.md  src
-$ ls
-here
-a  inc  Makefile  minishell  obj  README.md  src
-$
-=================================================================
-==7372==ERROR: LeakSanitizer: detected memory leaks
-
-Direct leak of 32 byte(s) in 1 object(s) allocated from:
-    #0 0x4c6233 in malloc (/home/alien/documents/42/cursus_2021/min_try_hd/minishell+0x4c6233)
-    #1 0x7fad3e31a4e8 in xmalloc (/lib/x86_64-linux-gnu/libreadline.so.7+0x3a4e8)
-
-SUMMARY: AddressSanitizer: 32 byte(s) leaked in 1 allocation(s).
-
-/************************************************************
-************************************************************/
-
- ls | <<d cat | cat -e > b
+ls | <<d cat | cat -e > b
 inc  leaks  Makefile  minishell  obj  README.md  src
 s
 d
@@ -36,10 +6,6 @@ cat: -: input file is output file
 
 /************************************************************
 ************************************************************/
-
-*
-05/02:
-* protect readline with strlen (no longer than intmax ?)
 * free char *prompt ?
 * cd => go to HOME
 * cd - => go to OLDPWD
@@ -69,3 +35,8 @@ SIGINT => exit status code ?
 /************************************************************
 ************************************************************/
 desactivate ./minishell arg ?
+
+/************************************************************
+************************************************************/
+colors:
+https://tforgione.fr/posts/ansi-escape-codes/
