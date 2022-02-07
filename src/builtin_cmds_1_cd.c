74eb62c7c918b4918c9e9f706fc29333e04a8221
daisvke <daisuketanigawa@live.fr>
2022-02-07 05:06:53 +0100

    Correct parsing error with quotes

--- src/builtin_cmds_1_cd.c
+++ src/builtin_cmds_1_cd.c
@@ -6,7 +6,7 @@
 /*   By: dtanigaw <dtanigaw@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/06 10:07:43 by dtanigaw          #+#    #+#             */
-/*   Updated: 2022/02/07 01:55:01 by dtanigaw         ###   ########.fr       */
+/*   Updated: 2022/02/07 04:00:59 by dtanigaw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,18 +38,7 @@ void	ms_execute_cmd_cd(t_ms *ms_env, t_ppx *ppx_env, char *arg_path)
 			ppx_putstr_fd(path, STDERR_FILENO, MS_NONE);
 			write(STDERR_FILENO, ": No such file or directory\n", 28);
 		}
-		current_absolute_path = getcwd(NULL, 0);
-		if (current_absolute_path == NULL)
-			ms_exit_with_error_message(ms_env, 5);
-		node = ms_lst_get_node_with_the_same_key(ms_env->envp_lst, "PWD=");
-		new_path = ppx_join_three_str(\
-			ppx_env, "PWD", "=", current_absolute_path \
-		);
-		current_absolute_path = ms_free(current_absolute_path);
-		ms_lst_assign_entry_to_node(ms_env, node, new_path);
-		new_path = ms_free(new_path);
-		ms_get_new_path_for_prompt(\
-			ms_env, ms_env->envp_lst, &ms_env->cmd_prompt);
+		ms_update_prompt_when_home_is_unset(ms_env);
 	}
 	else
 	{
