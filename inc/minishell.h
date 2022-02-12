ce8274049687927e06432c8c835417b11556f758
daisvke <daisuketanigawa@live.fr>
2022-02-12 10:54:43 +0100

    Free prompt struct

--- inc/minishell.h
+++ inc/minishell.h
@@ -6,7 +6,7 @@
 /*   By: dtanigaw <dtanigaw@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/14 06:18:38 by dtanigaw          #+#    #+#             */
-/*   Updated: 2022/02/12 10:46:03 by dtanigaw         ###   ########.fr       */
+/*   Updated: 2022/02/12 10:51:13 by dtanigaw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -71,7 +71,7 @@ void		ms_convert_envp_into_linked_list(char *envp[], t_ms *env);
 t_env_lst	*ms_lst_get_node_with_the_same_key(t_env_lst *envp_lst, char *key);
 void		ms_lst_add_back(t_env_lst *head, t_env_lst *new);
 void		ms_lst_assign_entry_to_node(t_env_lst *node, char *entry);
-void		ms_lst_clear_list(t_env_lst **node);
+void		ms_lst_clear_list(t_env_lst *node);
 t_env_lst	*ms_lst_create_new_node(t_ms *env, char *data);
 void		ms_lst_del_node(t_env_lst *node);
 t_env_lst	*ms_lst_get_last_node(t_env_lst *node);
