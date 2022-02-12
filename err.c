c6405f16876b7c220820aff468bd5671b22f5ded
daisvke <daisuketanigawa@live.fr>
2022-02-12 03:35:48 +0100

    modify err.c

--- err.c
+++ err.c
@@ -3,15 +3,8 @@ inc  leaks  Makefile  minishell  obj  README.md  src
 s
 d
 cat: -: input file is output file
-
-/**********************/
-ms_macro => macro ?
-        => ms_macro + ppx_macro
 /**********************/
 SIGINT => exit status code ?
 /**********************/
 colors:
 https://tforgione.fr/posts/ansi-escape-codes/
-/**********************/
-format macros header
-
