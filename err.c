f90649852780766ae5a6dd5cdaee7c19fc111751
daisvke <daisuketanigawa@live.fr>
2022-02-12 03:36:23 +0100

    modify err.c

--- err.c
+++ err.c
@@ -1,9 +1,3 @@
-ls | <<d cat | cat -e > b
-inc  leaks  Makefile  minishell  obj  README.md  src
-s
-d
-cat: -: input file is output file
-/**********************/
 SIGINT => exit status code ?
 /**********************/
 colors:
